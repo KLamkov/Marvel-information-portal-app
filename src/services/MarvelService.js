8e1a8d5e51e8214a3872eaa3bc3772fb70c5a6db
KLamkov <kirilllamkov@gmail.com>
2022-10-27 22:18:09 +0300

    optimized with FSM

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,7 +1,7 @@
 import {useHttp} from '../hooks/http.hook';
 
 const useMarvelService = () => {
-    const {loading, request, error, clearError} = useHttp();
+    const { request, clearError, process, setProcess} = useHttp();
 
     const _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     const _apiKey = 'apikey=34cb32fe02aaa3502799e85e4323f51b';
@@ -56,7 +56,14 @@ const useMarvelService = () => {
         }
     }
 
-    return {loading, error, clearError, getAllCharacters, getCharacterByName, getCharacter, getAllComics, getComic}
+    return {clearError, 
+            getAllCharacters, 
+            getCharacterByName, 
+            getCharacter, 
+            getAllComics, 
+            getComic, 
+            process,
+            setProcess}
 }
 
 export default useMarvelService;
